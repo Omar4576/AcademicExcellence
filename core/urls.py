df0e710bba2afc14b08b28ae445cf9c4af0a5b65
Zulfugar028 <gfotograf77@gmail.com>
2026-06-02 23:01:02 +0400

    Changes

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
