ecf5eb65f88a8477002e38c14a5b7fcb20279ff8
Zulfugar028 <gfotograf77@gmail.com>
2026-06-05 00:09:59 +0400

    Changes

--- core/settings.py
+++ core/settings.py
@@ -14,9 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'accounts',
     'orders',
 ]
@@ -64,14 +62,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'do5lgnq7g',
-    'API_KEY': '516931114741825',
-    'API_SECRET': 'TPTQeAZ3MMpyYpLIF9cjg6ROpU4',
-}
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
