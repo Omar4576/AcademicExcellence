dbf321a7af80200a9699d1fe8ad4c3940dc4917c
Zulfugar028 <gfotograf77@gmail.com>
2026-06-08 15:51:27 +0400

    Changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    # ==================== PASSWORD RESET ====================
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt',
+        success_url='/accounts/password_reset/done/',
+    ), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -93,6 +93,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = 'huseynlizulfugar@gmail.com'
-EMAIL_HOST_PASSWORD = 'kzhkkvvygvwgfzcy'
-DEFAULT_FROM_EMAIL = 'huseynlizulfugar@gmail.com'
+EMAIL_HOST_PASSWORD = 'aribeguo zqyfepch'   # ← Boşluqları silib belə yazın
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
